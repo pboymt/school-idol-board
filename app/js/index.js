547cad388f978c9ed5565b5cdc5969b3f12fa2ee
pboymt <pboymt@foxmail.com>
2016-03-28 17:04:44 +0800

    删除启动时多余的图片加载步骤，添加loadConfig备用

--- app/js/index.js
+++ app/js/index.js
@@ -5,47 +5,20 @@ const remote = electron.remote;
 const ipcRenderer = electron.ipcRenderer;
 const sib = require('./js/sib.js');
 
-// var mlist = function mlist() {
-//   var xhr = new XMLHttpRequest();
-//   xhr.open("GET", "./data/music.json", false);
-//   xhr.send();
-//   var obj = JSON.parse(xhr.responseText);
-//   console.log(obj);
-//   for (var i = 3; i >= 0; i--) {
-//     var item = document.createElement('div');
-//     item.className = 'music-item';
-//     item.textContent = obj[i]['name'];
-//     item.setAttribute('mid', obj[i]['mid']);
-//     document.getElementsByClassName('music-list')[0].appendChild(item);
-//     item.addEventListener('click', function() {
-//       console.log(this.getAttribute('mid'));
-//       sib.loadMusic(this.getAttribute('mid'));
-//     });
-//   }
-//   for (var x = obj.length - 1; x > 3; x--) {
-//     var item = document.createElement('div');
-//     item.className = 'music-item';
-//     item.textContent = obj[x]['name'];
-//     item.setAttribute('mid', obj[x]['mid']);
-//     document.getElementsByClassName('music-list')[0].appendChild(item);
-//     item.addEventListener('click', function() {
-//       console.log(this.getAttribute('mid'));
-//       sib.loadMusic(this.getAttribute('mid'));
-//     });
-//   }
-//   console.log('ok');
-//   document.getElementsByClassName('music-list')[0].addEventListener('mousewheel', function() {
-//     if (event.deltaY > 0) {
-//       var la = this.lastChild;
-//       this.removeChild(la);
-//       this.insertBefore(la, this.childNodes[0]);
-//     } else {
-//       var fi = this.firstChild;
-//       this.removeChild(fi);
-//       this.appendChild(fi);
-//     }
-//   });
-// };
+function loadConfig(){
+  if(!window.localStorage.config){
+    let defaultConfig = {
+      "autoPlayAfterClickList":true,
+      "useCustomPackage":false,
+      "musicListFile":["./data/muisc.json"],
+      "userDefalutPackage":"default"
+    };
+    window.localStorage.config = JSON.stringify(defaultConfig);
+    return defaultConfig;
+  }else{
+    return JSON.parse(window.localStorage.config);
+  }
+};
 
 function autohideMouse() {
   var isMoving = true;
@@ -66,27 +39,13 @@ function autohideMouse() {
 };
 
 window.onload = function() {
-  var singers = document.getElementsByClassName('singer');
   var mainBtn = document.getElementsByClassName('main-btn');
   var ctrlBtn = document.getElementsByClassName('ctrl-btn');
-  var s = new sib();
-  var deMainBtn = [{
-    width: 1920,
-    value: '1920*1080'
-  }, {
-    width: 1366,
-    value: '1280*720'
-  }, {
-    width: 1366,
-    value: '1280*400'
-  }, {
-    width: 1920,
-    value: 'large'
-  }]
-  for (var i = 0; i < singers.length; i++) {
-    //console.log(singers[i].classList[1]);
-    singers[i].style.backgroundImage = 'url(./data/package/default/' + singers[i].classList[1] + '.jpg)';
-  }
+  var s = new sib(loadConfig());
+  // for (var i = 0; i < singers.length; i++) {
+  //   //console.log(singers[i].classList[1]);
+  //   singers[i].style.backgroundImage = 'url(./data/package/default/' + singers[i].classList[1] + '.jpg)';
+  // }
   setTimeout(function() {
     document.getElementsByClassName('singers')[0].classList.add('fade');
   }, 500);
