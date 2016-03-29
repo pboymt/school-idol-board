9a0d70e37c9468724bbfaa52fdfac82a7b582150
pboymt <pboymt@foxmail.com>
2016-03-29 22:43:33 +0800

    打包脚本

---
+++ pkg.js
@@ -0,0 +1,38 @@
+'use strict';
+var rcedit = require('rcedit');
+var packager = require('electron-packager');
+var pkgjson = require('./package.json')
+var appVer = pkgjson.version;
+var appName = 'SIB';
+var path = require('path');
+var iconPath = path.join('resources', 'icon@512.ico');
+var outPath = path.join('build/release', appVer);
+var rcopt = {
+  'icon': iconPath
+}
+console.log(iconPath);
+var options = {
+  'arch': 'ia32',
+  'dir': '.',
+  'platform': 'win32',
+  'app-copyright': 'pboymt',
+  'app-version': appVer,
+  'asar': true,
+  'name': appName,
+  //'build-version':'0.1.1',
+  //'icon':iconPath,
+  'ignore': 'build',
+  'out': outPath,
+  'overwrite': true
+}
+packager(options, function(err, appPath) {
+  console.log('fuck');
+  console.log(path.join(appPath[0], appName + '.exe'));
+  rcedit(path.join(appPath[0], appName + '.exe'), rcopt, function(err) {
+    if (!err) {
+      console.log('Success!');
+    } else {
+      console.log(err);
+    }
+  })
+});
