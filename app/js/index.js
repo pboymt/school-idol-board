425bd1332b1eab108d468dad9982b132c3bee213
pboymt <pboymt@foxmail.com>
2016-04-07 22:28:44 +0800

    增加设置功能

--- app/js/index.js
+++ app/js/index.js
@@ -29,23 +29,59 @@ function autohideMouse() {
   })
 };
 
+function setConfig(sibobj) {
+  var conf = new electronConfig();
+  var configEle = document.querySelectorAll('.config-value');
+  var configs = conf.getAll();
+  for (var x = 0; x < configEle.length; x++) {
+    console.log(configs[configEle[x].getAttribute('name')]);
+    if (configEle[x].getAttribute('type') == 'checkbox') {
+      configEle[x].checked = configs[configEle[x].getAttribute('name')];
+    } else {
+      configEle[x].setAttribute('value', configs[configEle[x].getAttribute('name')]);
+    }
+    configEle[x].addEventListener('change', function() {
+      console.log(this.checked);
+      if (this.getAttribute('type') == 'checkbox') {
+        conf.set(this.getAttribute('name'), this.checked, function(c) {
+          console.log('重设成功！');
+          sibobj.config = c;
+        });
+      } else {
+
+      }
+    });
+  }
+};
+
 window.onload = function() {
   var mainBtn = document.getElementsByClassName('main-btn');
   var ctrlBtn = document.getElementsByClassName('ctrl-btn');
   var s = new sib(loadConfig());
+  //console.log(s.config);
+  setConfig(s);
   var beforeSize = 'default';
   setTimeout(function() {
     document.getElementsByClassName('singers')[0].classList.add('fade');
   }, 500);
   //autohideMouse();
-  mainBtn[0].addEventListener('change', function() {
+  mainBtn[0].addEventListener('click', function() {
+    var configFrame = document.querySelector('.config');
+    if (configFrame.style.display == 'none') {
+      configFrame.style.display = 'block';
+    } else {
+      configFrame.style.display = 'none';
+    }
+    //console.log(this.value);
+  });
+  mainBtn[1].addEventListener('change', function() {
     if (this.value != 'full') {
       beforeSize = this.value;
     }
     ipcRenderer.send('changeWindow', this.value);
     //console.log(this.value);
   });
-  mainBtn[1].addEventListener('click', function() {
+  mainBtn[2].addEventListener('click', function() {
     ipcRenderer.send('closeWindow');
     //console.log(this.value);
   });
